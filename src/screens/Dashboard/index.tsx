7831c13349bad0bb4f45725b6e8b94e79d431c65
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2022-10-20 13:45:55 -0300

    Starting header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import { Container, Title } from './styles'
+import { 
+    Container, 
+    Header, 
+    UserWrapper, 
+    UserInfo, 
+    Photo, 
+    User, 
+    UserGreeting, 
+    UserName, 
+} from './styles'
 
 export function Dashboard(){
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri: "https://avatars.githubusercontent.com/u/16305348?v=4"}} />
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Marcelo</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
+            </Header>
         </Container>
     )
 }
